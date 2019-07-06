ae185cf79765aba6f24a454099c3b359aa2b877d
Javier Gomez <javiergomezve@gmail.com>
2019-07-06 11:26:21 -0500

    [add] Integrating front-end

---
+++ src/build/precache-manifest.40c12f5b4bf4211a197d9042d12ce390.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffebc3fad57c0e43a056013d7fc20ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "446b4e229caa3b70709b",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "d212323b7b89d0526861",
+    "url": "/static/js/2.8a17f4b0.chunk.js"
+  },
+  {
+    "revision": "446b4e229caa3b70709b",
+    "url": "/static/js/main.4f51d102.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
